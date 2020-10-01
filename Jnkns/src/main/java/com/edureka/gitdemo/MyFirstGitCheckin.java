89f95eba354ed26ca2eff06db912cf50e653639a
NeelamDwi23 <ndwivedi2007@gmail.com>
2020-10-01 05:32:35 -0400

    new class

---
+++ Jnkns/src/main/java/com/edureka/gitdemo/MyFirstGitCheckin.java
@@ -0,0 +1,12 @@
+package com.edureka.gitdemo;
+
+public class MyFirstGitCheckin {
+
+	public static void main(String[] args)
+	{
+		System.out.println("learning Git");
+		System.out.println("adding lines");
+
+	}
+
+}
